055fce6eb5ba560f0029fba06fc50c5ac52b2927
Alex Dunne <alex12dunne@gmail.com>
2021-10-07 00:57:10 -0400

    first ident tests, building patterns, using macros to deserialize into struct

--- src/ident.rs
+++ src/ident.rs
@@ -1,8 +1,48 @@
 pub use crate::client::ApiClient;
+use serde::{Serialize, Deserialize};
 
 pub struct Ident {
     pub client: ApiClient
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct ApplicationConfig {
+    network_id: Option<String>,
+    baselined: Option<bool>,
+    webhook_secret: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Application {
+    id: String,
+    created_at: String,
+    network_id: String,
+    user_id: String,
+    name: String,
+    description: Option<String>,
+    r#type: Option<String>,
+    config: ApplicationConfig,
+    hidden: bool
+}
+
+// pub struct User {
+//     id: String,
+//     created_at: String,
+//     name: String,
+//     first_name: String,
+//     last_name: String,
+//     email: String,
+//     permissions: i32,
+//     privacy_policy_agreed_at: String,
+//     terms_of_service_agreed_at: String
+// }
+
+// pub struct Token {
+//     id: String,
+//     expires_in: i32,
+//     token: String,
+//     permissions: i32
+// }
 
 impl Ident {
     pub fn init(scheme: Option<String>, host: Option<String>, token: Option<String>) -> Result<Self, ()> {
@@ -10,7 +50,7 @@ impl Ident {
         let _host = host.unwrap_or(String::from("ident.provide.services"));
 
         let client = ApiClient::init(Some(_scheme), Some(_host), Some(String::from("api/v1")), token).expect("ident api client");
-        Ok( Self { client })
+        Ok( Self { client } )
     }
 
     // applications
@@ -19,20 +59,20 @@ impl Ident {
         Ok( res )
     }
 
-    pub async fn applications(&self, params: serde_json::Value) -> Result<reqwest::Response, reqwest::Error> {
+    pub async fn get_applications(&self, params: serde_json::Value) -> Result<reqwest::Response, reqwest::Error> {
         let res = self.client.get(String::from("applications"), params).await?;
         Ok( res )
     }
     
     pub fn associate_user_with_application() {}
 
-    pub async fn application(&self, application_id: String, params: serde_json::Value) -> Result<reqwest::Response, reqwest::Error> {
+    pub async fn get_application(&self, application_id: String, params: serde_json::Value) -> Result<reqwest::Response, reqwest::Error> {
         let uri = format!("applications/{}", application_id);
         let res = self.client.get(uri, params).await?;
         Ok( res )
     }
 
-    pub fn application_users() {}
+    pub fn get_application_users() {}
 
     pub async fn update_application(&self, application_id: String, params: serde_json::Value) -> Result<reqwest::Response, reqwest::Error> {
         let uri = format!("applications/{}", application_id);
@@ -43,16 +83,16 @@ impl Ident {
     pub fn delete_application() {}
 
     // organizations
-    pub fn organizations() {}
+    pub fn get_organizations() {}
 
     pub fn create_organization() {}
 
-    pub fn organization() {}
+    pub fn get_organization() {}
 
     pub fn update_organization() {}
 
     // tokens
-    pub fn tokens() {}
+    pub fn get_tokens() {}
 
     pub fn revoke_token() {}
 
@@ -64,13 +104,13 @@ impl Ident {
     }
 
     // users
-    pub fn users() {}
+    pub fn get_users() {}
     
     pub fn create_user() {}
 
     pub fn update_user() {}
 
-    pub fn user() {}
+    pub fn get_user() {}
     
     pub fn delete_user() {}
 }
@@ -80,9 +120,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn ident_client() {
+    fn test_ident_client_init() {
         let ident = Ident::init(None, None, Some(String::from(""))).expect("ident client");
 
         assert_eq!(ident.client.base_url, String::from("https://ident.provide.services/api/v1"))
     }
+
+    
+
+    #[tokio::test]
+    async fn test_ident_application_suite() {
+        let token = std::env::var("ACCESS_TOKEN").expect("access token");
+        let ident = Ident::init(None, None, Some(token)).expect("ident application client");
+
+        let create_app_params = serde_json::json!({
+            "name": "rust test application"
+        });
+        let create_app_res = ident.create_application(create_app_params).await.expect("ident create application res");
+        assert_eq!(create_app_res.status(), 201);
+
+        let create_app_body: Application = create_app_res.json::<Application>().await.expect("ident create application body");
+        assert_eq!(create_app_body.name, String::from("rust test application"));
+    }
+
+    // #[tokio::test]
+    // async fn test_ident_token_suite() {
+
+    // }
 }
