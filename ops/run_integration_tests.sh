9412950b1a6200c673f160aa60713bed51ae826c
Alex Dunne <alex12dunne@gmail.com>
2021-10-23 04:32:37 -0400

    Running all tests, typo

--- ops/run_integration_tests.sh
+++ ops/run_integration_tests.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# docker network prune # only prune these networks
 docker-compose -f ./ops/docker-compose.yml build --no-cache
 docker-compose -f ./ops/docker-compose.yml up -d
 sleep 20
-IDENT_API_HOST=localhost:8081 IDENT_API_SCHEME=http VAULT_API_HOST=localhost:8082 VAULT_API_SCHEME=http PRIVACY_API_HOST=localhost:8083 PRIVACY_API_SCHEME=http NCHAIN_API_HOST=localhost:8084 NCHAIN_API_SCHEME=http BASELINE_API_HOST=localhost:8085 BASELINE_API_SCHEME=http cargo test baseline -- --test-threads=1
+IDENT_API_HOST=localhost:8081 IDENT_API_SCHEME=http VAULT_API_HOST=localhost:8082 VAULT_API_SCHEME=http PRIVACY_API_HOST=localhost:8083 PRIVACY_API_SCHEME=http NCHAIN_API_HOST=localhost:8084 NCHAIN_API_SCHEME=http BASELINE_API_HOST=localhost:8085 BASELINE_API_SCHEME=http cargo test -- --test-threads=1
 rm .local-baseline-test-config.yaml
 docker-compose -f ./ops/docker-compose.yml down
 docker volume rm ops_provide-db
-prvd baseline stack stop --name $(jq '.org_name' test-config.json | xargs)
-rm .test-config.json # make this dotfile?
-# docker network rm $(jq '.org_name' test-config.json | xargs)
-# remove baseline db?
-# how to handle sometimes having to docker network prune
+prvd baseline stack stop --name $(jq '.org_name' .test-config.json | xargs)
+rm .test-config.json
