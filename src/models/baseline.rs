b93d07dee1eac6b1b8106075f752d47c09a64184
Alex Dunne <alex12dunne@gmail.com>
2021-12-25 23:16:06 -0500

    annotations, commenting out unused structs

--- src/models/baseline.rs
+++ src/models/baseline.rs
@@ -2,36 +2,36 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use crate::models::privacy::Circuit;
 
-pub struct BaselineContext {
-	id: Option<String>,
-	baseline_id: Option<String>,
-	records: Option<Vec<BaselineRecord>>,
-	workflow: Option<Workflow>,
-	workflow_id: Option<String>,
-}
+// pub struct BaselineContext {
+// 	id: Option<String>,
+// 	baseline_id: Option<String>,
+// 	records: Option<Vec<BaselineRecord>>,
+// 	workflow: Option<Workflow>,
+// 	workflow_id: Option<String>,
+// }
 
-pub struct BaselineRecord {
-	id: Option<String>,
-	baseline_id: Option<String>,
-	context: Option<BaselineContext>,
-	context_id: Option<String>,
-	r#type: Option<String>,
-}
+// pub struct BaselineRecord {
+// 	id: Option<String>,
+// 	baseline_id: Option<String>,
+// 	context: Option<BaselineContext>,
+// 	context_id: Option<String>,
+// 	r#type: Option<String>,
+// }
 
-pub struct Config {
-	counterparties: Option<Vec<Participant>>,
-	env: Value,
-	errors: Value,
-	network_id: Option<String>,
-	organization_address: Option<String>,
-	organization_id: Option<String>,
-	organization_refresh_token: Option<String>,
-	registry_contract_address: Option<String>,
-}
+// pub struct Config {
+// 	counterparties: Option<Vec<Participant>>,
+// 	env: Value,
+// 	errors: Value,
+// 	network_id: Option<String>,
+// 	organization_address: Option<String>,
+// 	organization_id: Option<String>,
+// 	organization_refresh_token: Option<String>,
+// 	registry_contract_address: Option<String>,
+// }
 
-pub struct VerifiableCredential {
-    credential: Option<String>,
-}
+// pub struct VerifiableCredential {
+//     credential: Option<String>,
+// }
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct Mapping {
@@ -74,17 +74,17 @@ pub struct MappingModel {
     mapping_id: String,
 }
 
-pub struct Message {
-	id: Option<String>,
-	baseline_id: Option<String>,
-	errors: Value,
-	message_id: Option<String>,
-	payload: Value,
-	protocol_message: Option<ProtocolMessage>,
-	recipients: Option<Vec<Participant>>,
-	status: Option<String>,
-	r#type: Option<String>,
-}
+// pub struct Message {
+// 	id: Option<String>,
+// 	baseline_id: Option<String>,
+// 	errors: Value,
+// 	message_id: Option<String>,
+// 	payload: Value,
+// 	protocol_message: Option<ProtocolMessage>,
+// 	recipients: Option<Vec<Participant>>,
+// 	status: Option<String>,
+// 	r#type: Option<String>,
+// }
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct Participant {
@@ -99,24 +99,24 @@ pub struct Participant {
 }
 
 
-pub struct ProtocolMessage {
-    baseline_id: Option<String>,
-    opcode: Option<String>,
-    sender: Option<String>,
-    recipient: Option<String>,
-    shield: Option<String>,
-    identifier: Option<String>,
-    signature: Option<String>,
-    r#type: Option<String>,
-    payload: Option<ProtocolMessagePayload>,
-}
+// pub struct ProtocolMessage {
+//     baseline_id: Option<String>,
+//     opcode: Option<String>,
+//     sender: Option<String>,
+//     recipient: Option<String>,
+//     shield: Option<String>,
+//     identifier: Option<String>,
+//     signature: Option<String>,
+//     r#type: Option<String>,
+//     payload: Option<ProtocolMessagePayload>,
+// }
 
-pub struct ProtocolMessagePayload {
-    object: Value,
-    proof: Option<String>,
-    r#type: Option<String>,
-    witness: Value,
-}
+// pub struct ProtocolMessagePayload {
+//     object: Value,
+//     proof: Option<String>,
+//     r#type: Option<String>,
+//     witness: Value,
+// }
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 pub struct Workgroup {
