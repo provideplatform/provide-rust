f27494735643d53a2002a62565e16011515c98c6
Alex Dunne <alex12dunne@gmail.com>
2022-05-05 23:40:57 -0400

    run_many wip

--- ops/run_many.sh
+++ ops/run_many.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-
+if [[ $# != 1 ]]; then
+    echo "must pass desired number of test runs"
+    exit 0
+fi
 
 echo "Running $1 tests..."
-echo "Loading results into $2..."
 
 i=0
 while [[ $i -lt $1 ]]; do
-    mkdir -p ~/Documents/provide-rust-runs/$i
-    SUITE="baseline" CONTAINER_REGEX="organization-api" ./ops/run_integration_tests.sh ~/Documents/provide-rust-runs/$i/docker_output.txt ~/Documents/provide-rust-runs/$i/test_output.txt --with-registry-contract-address --bounce-docker
+    mkdir -p test-output/$i
+    SUITE="baseline" CONTAINER_REGEX="organization-api" OUTPUT_DIR="test-output/$i" ./ops/run_integration_tests.sh --with-registry-contract-address
     ((i++))
 done
